8890f042e4e2cee4de6256788f5003bcf984c244
Sam Miller <smiller2033@outlook.com>
2023-12-01 16:59:17 -0600

    push current state

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,14 +1,6 @@
 use std::fs::read_to_string;
 
 
-
-// fn read<R: Read>(io: R) -> Result<Vec<str>, Error> {
-//     let br = BufReader::new(io);
-//     br.lines()
-//         .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
-//         .collect()
-// }
-
 fn read(filename: &str) -> Vec<String> {
     let mut result = Vec::new();
 
@@ -19,6 +11,43 @@ fn read(filename: &str) -> Vec<String> {
     result
 }
 
+
+fn process_line(line: &str) -> u32 {
+    let mut it = (0..line.len()).filter_map(|index| {
+        let reduced_line = &line[index..];
+        let result = if reduced_line.starts_with("one") {
+            '1'
+        }else if reduced_line.starts_with("two"){
+            '2'
+        }else if reduced_line.starts_with("three"){
+            '3'
+        }else if reduced_line.starts_with("four"){
+            '4'
+        }else if reduced_line.starts_with("five"){
+            '5'
+        }else if reduced_line.starts_with("six"){
+            '6'
+        }else if reduced_line.starts_with("seven"){
+            '7'
+        }else if reduced_line.starts_with("eight"){
+            '8'
+        }else if reduced_line.starts_with("nine"){
+            '9'
+        } else {
+            reduced_line.chars().next().unwrap()
+        };
+    
+        result.to_digit(10)
+    });
+    let first = it.next().expect("should be a number");
+
+    match it.last() {
+        Some(num) => format!("{first}{num}"),
+        None => format!("{first}{first}"),
+    }
+    .parse::<u32>()
+    .expect("should be a valid number")
+}
 
 
 pub fn pt1() -> Option<i64> {
@@ -58,7 +87,11 @@ pub fn pt1() -> Option<i64> {
 pub fn pt2() -> Option<i64> {
     let vec = read("./input");
     let mut sum: i64 = 0;
-    let mut callibration: Vec<i64> = vec![];
+
+
+    // Make lookup table
+    // fixup the vec to parse out the numbers
+
 
     for _item in vec{
         let mut first: char= 'a';
@@ -76,13 +109,11 @@ pub fn pt2() -> Option<i64> {
         if first.is_numeric() && last.is_numeric() {
             let cal_str: String = format!("{}{}",first,last);
             let cal_num: i64 = cal_str.parse::<i64>().unwrap();
-            callibration.push(cal_num);
             sum += &cal_num;
         }
         else if first.is_numeric() {
             let cal_str: String = format!("{}{}",first,first);
             let cal_num: i64 = cal_str.parse::<i64>().unwrap();
-            callibration.push(cal_num);
             sum += &cal_num;
         }
 
